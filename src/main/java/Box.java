e9ceb18afca13cba0a183cddc16b6a1005e906ef
Tararkov Andrey <gfhflbuvf46@mail.ru>
2019-03-28 16:38:00 +0500

    исправлено

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -6,16 +6,16 @@ public class Box {
 
         double volume = 0;
 
-        double approximateResult = 0;
+        double approximateHeight = 0;
 
         for (double wallHeight = 0; wallHeight < min / 2; wallHeight += 1) {
             double currentVolume = wallHeight * (length - 2 * wallHeight) * (width - 2 * wallHeight);
             if (currentVolume > volume) {
-                approximateResult = currentVolume;
+                approximateHeight = wallHeight;
             }
-        }
+    }
 
-        for (double wallHeight = approximateResult - 1; wallHeight < approximateResult + 1; wallHeight += 0.001) {
+        for (double wallHeight = approximateHeight - 1; wallHeight < approximateHeight + 1; wallHeight += 0.001) {
             double currentVolume = wallHeight * (length - 2 * wallHeight) * (width - 2 * wallHeight);
             if (currentVolume > volume) {
                 volume = currentVolume;
